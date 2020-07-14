1e6d18245cf4259eac2a7543f7581a8c907ce6a3
Jacquelyn <49626969+jacquelynra@users.noreply.github.com>
2020-07-13 21:06:02 -0300

    Update distribution, change language on uicore and version update

--- app/assets/js/scripts/uicore.js
+++ app/assets/js/scripts/uicore.js
@@ -26,9 +26,7 @@ window.eval = global.eval = function () {
 
 // Display warning when devtools window is opened.
 remote.getCurrentWebContents().on('devtools-opened', () => {
-    console.log('%cThe console is dark and full of terrors.', 'color: white; -webkit-text-stroke: 4px #a02d2a; font-size: 60px; font-weight: bold')
-    console.log('%cIf you\'ve been told to paste something here, you\'re being scammed.', 'font-size: 16px')
-    console.log('%cUnless you know exactly what you\'re doing, close this window.', 'font-size: 16px')
+    console.log('%cSolo usa esta consola en caso de reportar errores. No introduzcas nada que no sepas exactamente *qué* significa.', 'font-size: 16px')
 })
 
 // Disable zoom, needed for darwin.
